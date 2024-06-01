d38751e7de0773f7904a4bc9a2dd673f2aa5ffb2
Zo Tiana ANDRIAMIFIDY <zotianaandriamifidy@gmail.com>
2024-06-01 13:28:45 +0300

    update js file

--- src/script.js
+++ src/script.js
@@ -1,6 +1,46 @@
-function searchCity(event) {
+function searchCity(city) {
+  let apiKey = "d73d01c748aatfb2o0bcd43d19416a3e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function refreshWeather(response) {
+  console.log(response.data);
+}
+function searchUpdate(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = searchInput.value;
+  let currentTimeElement = document.querySelector("#current-time");
+  currentTimeElement.innerHTML = formattedDate();
+  searchCity(searchInput.value);
+}
+
+function formattedDate() {
+  let time = new Date();
+  let hour = time.getHours();
+  let minute = time.getMinutes();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[time.getDay()];
+  let currentTime = `${day} ${hour}:${minute} `;
+  return currentTime;
 }
 
 let form = document.querySelector("form");
-form.addEventListener("submit", searchCity);
+let searchButton = document.querySelector("#submit-button");
+form.addEventListener("submit", searchUpdate);
